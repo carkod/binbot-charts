8b5fda47b6ba19bac4b769daea3a5e0258c5a454
Carlos Wu <carlos.fei@canonical.com>
2022-09-04 16:42:01 +0100

    Update to react hooks component

--- src/components/TVChartContainer.jsx
+++ src/components/TVChartContainer.jsx
@@ -1,59 +1,38 @@
-import React, { PureComponent } from "react";
+import React, { useEffect, useRef } from "react";
 import { widget } from "../charting_library";
 import Datafeed from "./datafeed";
 
-export default class TVChartContainer extends PureComponent {
-  static defaultProps = {
-    symbol: "AAPL",
-    interval: "D",
-    datafeedUrl: "https://demo_feed.tradingview.com",
-    libraryPath: "/charting_library/",
-    chartsStorageUrl: "https://saveload.tradingview.com",
-    chartsStorageApiVersion: "1.1",
-    clientId: "tradingview.com",
-    userId: "public_user_id",
-    fullscreen: false,
-    autosize: true,
-    studiesOverrides: {},
-  };
+export default function TVChartContainer ({
+  symbol= "AAPL",
+  interval= "D",
+  libraryPath= "/charting_library/",
+  timescaleMarks=[],
+  orderLines=[],
+}) {
+  const containerRef = useRef(null);
 
-  tvWidget = null;
-
-  constructor(props) {
-    super(props);
-
-    this.ref = React.createRef();
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const widgetOptions = {
-      symbol: this.props.symbol,
-      // BEWARE: no trailing slash is expected in feed URL
-      datafeed: new Datafeed(this.props.timescaleMarks),
-      interval: this.props.interval,
-      container: this.ref.current,
-      library_path: this.props.libraryPath,
+      symbol: symbol,
+      datafeed: new Datafeed(timescaleMarks),
+      interval: interval,
+      container: containerRef.current,
+      library_path: libraryPath,
       locale: "en",
-      fullscreen: this.props.fullscreen,
-      autosize: this.props.autosize,
-      studies_overrides: this.props.studiesOverrides,
-    };
-
+      fullscreen: false,
+      autosize: true,
+      studies_overrides: {},
+    }
     const tvWidget = new widget(widgetOptions);
-    this.tvWidget = tvWidget;
 
     tvWidget.onChartReady(() => {
-      this.props.orderLines.forEach(element => {
+      orderLines.forEach(element => {
         tvWidget.chart().createPositionLine()
         .setText(element.text)
         .setTooltip(element.tooltip)
-        // .setProtectTooltip("Protect position")
-        // .setCloseTooltip("Close position")
-        // .setReverseTooltip("Reverse position")
         .setQuantity(element.quantity)
         .setQuantityBackgroundColor(element.color)
         .setQuantityBorderColor(element.color)
-        // .setExtendLeft(false)
         .setLineStyle(0)
         .setLineLength(25)
         .setLineColor(element.color)
@@ -61,19 +40,16 @@ export default class TVChartContainer extends PureComponent {
         .setBodyTextColor(element.color)
         .setPrice(element.price)
       });
-      
-    })
-
-  }
-
-  componentWillUnmount() {
-    if (this.tvWidget !== null) {
-      this.tvWidget.remove();
-      this.tvWidget = null;
+    });
+  
+    // returned function will be called on component unmount 
+    return () => {
+      if (this.tvWidget !== null) {
+        this.tvWidget.remove();
+        this.tvWidget = null;
+      }
     }
-  }
+  }, [])
 
-  render() {
-    return <div ref={this.ref} style={{ height: "calc(100vh)" }} />;
-  }
+  return <div ref={containerRef} style={{ height: "calc(100vh)" }} />;
 }
