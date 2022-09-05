5d482cbdaa119a616b4af100fb01788e79da9f08
Carlos Wu <carlos.fei@canonical.com>
2022-09-05 23:15:06 +0100

    v0.1.6

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 import TVChartContainer from "./TVChartContainer";
 import { IOrderLine, IWidgetOptions, ITimescaleMarks } from "./charting-library-interfaces";
-export { TVChartContainer, IOrderLine, IWidgetOptions, ITimescaleMarks };
+export { TVChartContainer };
